4a347bd01f1bf03a8432206ab3f77b494ed5c3ff
Rafael Gomes <rgomes@thoughtworks.com>
2016-01-17 11:39:38 -0200

    Simplificando o deploy de redis

--- deploy_redis.sh
+++ deploy_redis.sh
@@ -2,12 +2,5 @@
 
 DOCKER=/usr/bin/docker
 
-$DOCKER inspect redis-${ENV_APP}
-
-if [ $? == 0 ]; then
-
-  $DOCKER rm redis-${ENV_APP}
-
-fi
-
+$DOCKER rm redis-${ENV_APP}
 $DOCKER run -d -v dados:/data --name redis-${ENV_APP} redis
