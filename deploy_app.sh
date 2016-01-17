53896e565a777216806416c6d6ad37f2ca1ec1e2
Rafael Gomes <rgomes@thoughtworks.com>
2016-01-17 12:25:29 -0200

    Adicionado a variavel no deploy de app

--- deploy_app.sh
+++ deploy_app.sh
@@ -3,4 +3,4 @@
 DOCKER=/usr/bin/docker
 
 $DOCKER rm app-${ENV_APP}
-$DOCKER run -d -p ${PYTHON_PORT}:${PYTHON_PORT} --link redis-${ENV_APP} --name app-${ENV_APP} ${IMAGEM}
+$DOCKER run -d -p ${PYTHON_PORT}:${PYTHON_PORT} --link redis-${ENV_APP} --name app-${ENV_APP} --env-file variaveis ${IMAGEM}
